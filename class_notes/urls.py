cc250e8f03e032734d1bb51739605667864fb819
Prudita Victory <prudita.victory@ui.ac.id>
2023-05-17 10:52:21 +0700

    Buat form

--- class_notes/urls.py
+++ class_notes/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from class_notes.views import show_note
+from class_notes.views import create_note
 
 app_name = 'class_notes'
 
 urlpatterns = [
     path('', show_note, name='show_note'),
+    path('create', create_note, name='create_note'),
 ]
--- class_notes/views.py
+++ class_notes/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
+from class_notes.models import Note
+from django.http import HttpResponseRedirect
+from class_notes.forms import NoteForm
+from django.urls import reverse
+
 def show_note(request):
-    return render(request, "note.html")
+    note = Note.objects.all()
+    context = {
+        'class_notes': note,
+        
+    }
+
+    return render(request, "note.html", context)
+
+def create_note(request):
+    form = NoteForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('class_notes:show_note'))
+
+    context = {'form': form}
+    return render(request, "create_note.html", context)
 
 
