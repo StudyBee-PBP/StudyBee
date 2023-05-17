4ae8eec4e001ca2b5a2748a3ff4cf47c72632012
Prudita Victory <prudita.victory@ui.ac.id>
2023-05-17 13:35:37 +0700

    xml dan json by id

--- class_notes/urls.py
+++ class_notes/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from class_notes.views import show_note
 from class_notes.views import create_note
 from class_notes.views import show_xml, show_json
+from class_notes.views import show_xml_by_id, show_json_by_id 
+
 app_name = 'class_notes'
 
 urlpatterns = [
@@ -9,4 +11,8 @@ urlpatterns = [
     path('create', create_note, name='create_note'),
     path('xml/', show_xml, name='show_xml'), 
     path('json/', show_xml, name='show_json'), 
+        
+    path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<int:id>', show_json_by_id, name='show_json_by_id'), 
+
 ]
--- class_notes/views.py
+++ class_notes/views.py
@@ -33,5 +33,12 @@ def show_json(request):
     data = Note.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
+def show_xml_by_id(request):
+    data = Note.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request):
+    data = Note.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 
